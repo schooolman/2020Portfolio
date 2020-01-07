d815e1bfc6dafc02cc4f80d16d160f3e8c35fec1
Jake Schoolmeesters <jake.schoolmeesters@icfolson.com>
2020-01-06 18:39:01 -0600

    updating portfolio to include recent work.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,11 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href="https://twitter.com/Schooolman" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://github.com/schooolman" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://github.com/schooolman" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                        <li><a href="https://codepen.io/schooolman" className="icon fa-codepen"><span className="label">Codepen</span></a></li>
                         <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
                     </ul>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,10 @@ class Header extends React.Component {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <div href="#" className="image avatar"><img src={avatar} alt="" /></div>
+                    <h1>Hey there! <br /> I'm Jake Schoolmeesters, a front&#8209;end&nbsp;developer
+                    based out of Chicago, Illinois.
+                    </h1>
                 </div>
                 <Footer />
             </header>
